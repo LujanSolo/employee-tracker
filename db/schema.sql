d8527ffc6785f8d2880e9000709d4ca398c2adab
LujanSolo <lujansolo@proton.me>
2022-09-26 15:45:18 -0700

    installed node packages (express, mysql2, inquirer). Created schema tables and DB

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,26 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
 USE employee_db;
+
+CREATE TABLE department (
+  id INT NOT NULL,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT NOT NULL
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT NOT NULL,
+  manager_id INT
+);
+
+SELECT * FROM employee_db;

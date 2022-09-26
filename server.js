ecce20b90400a5ee4a5513d58c61d2373c9ba8b9
LujanSolo <lujansolo@proton.me>
2022-09-26 16:14:16 -0700

    updated server.js

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const express = require('express');
+// Import and require mysql2
 const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
 const db = mysql.createConnection(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
   {
     host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'employee_db'
   },
-  console.log(`Connected to the database.`)
+  console.log(`Connected to the Employee Database.`)
 );

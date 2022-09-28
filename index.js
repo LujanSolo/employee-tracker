0a656b806896149918e4c53e9632e2f9beea6b2f
LujanSolo <lujansolo@proton.me>
2022-09-27 23:25:01 -0700

    added db.queries to add and update options so that CHOICES fed back DATA. Nested db.queries so that they fall in the correct sequential order per the related questions. Updated all tables with SELECT values so that table column headings would display actual titles and not just the index number of the title/name/employee

--- index.js
+++ index.js
@@ -30,7 +30,9 @@ const init = () => {
 
     if (task == "View All Roles") {
       db.promise()
-        .query("SELECT * FROM role")
+        .query(
+          "SELECT role.id, role.title, role.salary, department.name FROM role LEFT JOIN department ON role.department_id = department.id"
+        )
         .then((data) => {
           console.table(data[0]);
           init();
@@ -39,7 +41,9 @@ const init = () => {
 
     if (task == "View All Employees") {
       db.promise()
-        .query("SELECT * FROM employee")
+        .query(
+          "SELECT employee.id, employee.first_name, employee.last_name, role.title, concat(manager_table.first_name,' ', manager_table.last_name) Manager FROM employee LEFT JOIN role ON role.id = employee.role_id LEFT JOIN employee manager_table ON manager_table.id = employee.manager_id"
+        )
         .then((data) => {
           console.table(data[0]);
           init();
@@ -59,106 +63,105 @@ const init = () => {
     }
 
     if (task == "Add A Role") {
-      prompt([
-        {
-          type: "input",
-          name: "title",
-          message: "What is the name of the role?",
-        },
-        {
-          type: "number",
-          name: "salary",
-          message: "What is the salary of this role?"
-        },
-        {
-          type: "list",
-          name: "department_id",
-          message: "To which department does this role belong?",
-          choices: [
-            "1",
-            "2",
-            "3",
-            "4"
-          ],
-        },
-      ]).then((newRole) =>
-        db.promise().query("INSERT INTO role SET ?", newRole).then(init)
-      );
+      db.query("SELECT name, id value FROM department", (err, data) => {
+        prompt([
+          {
+            type: "input",
+            name: "title",
+            message: "What is the name of the role?",
+          },
+          {
+            type: "number",
+            name: "salary",
+            message: "What is the salary of this role?",
+          },
+          {
+            type: "list",
+            name: "department_id",
+            message: "To which department does this role belong?",
+            choices: data,
+          },
+        ]).then((newRole) =>
+          db.promise().query("INSERT INTO role SET ?", newRole).then(init)
+        );
+      });
     }
 
     if (task == "Add An Employee") {
-      prompt([
-        {
-          type: "input",
-          name: "first_name",
-          message: "What is the employee's first name?"
-        },
-        {
-          type: "input",
-          name: "last_name",
-          message: "What is the employee's last name?"
-        },
-        {
-          type: "list",
-          name: "role_id",
-          message: "What is the employee's role?",
-          choices: [
-            "Accountant",
-            "Graphic Artist",
-            "Lawyer",
-            "Legal Team Lead",
-            "Software Engineer"
-          ],
-        },
-        {
-          type: "list",
-          name: "manager_id",
-          message: "Who is the employee's manager?",
-          choices: [
-            "Jamie Lujan",
-            "Brandy Kolanko"
-          ],
-        },
-      ]).then((newEmployee) =>
-        db.promise().query("INSERT INTO employee SET ?", newEmployee).then(init)
-      );
+      db.query("SELECT title name, id value FROM role", (err, data) => {
+        db.query(
+          "SELECT concat(first_name,' ', last_name) name, id value FROM employee",
+          (err, empData) => {
+            prompt([
+              {
+                type: "input",
+                name: "first_name",
+                message: "What is the employee's first name?",
+              },
+              {
+                type: "input",
+                name: "last_name",
+                message: "What is the employee's last name?",
+              },
+              {
+                type: "list",
+                name: "role_id",
+                message: "What is the employee's role?",
+                choices: data,
+              },
+              {
+                type: "list",
+                name: "manager_id",
+                message: "Who is the employee's manager?",
+                choices: empData,
+              },
+            ]).then((newEmployee) =>
+              db
+                .promise()
+                .query("INSERT INTO employee SET ?", newEmployee)
+                .then(init)
+            );
+          }
+        );
+      });
     }
 
-
-    //* SET role_id = ?
-    //* WHERE id = ?
-    //* go to the EMPLOYEE TABLE, select the EMPLOYEE ID that user input after first question, then SET role_id to ROLE_ID CHOICES
     if (task == "Update An Employee Role") {
-      prompt([
-        {
-          type: "list",
-          name: "first_name",
-          message: "Whose role do you want to update?",
-          choices: [
-            "Jamie",
-            "Steven",
-            "Brandy",
-            "Mike",
-            "Melissa"
-          ],
-        },
-        {
-          type: "list",
-          name: "title",
-          message: "Which role would you like to assign to the selected employee?", //*possible to {{employee}}? or something?
-          choices: [
-            "1",
-            "2",
-            "3",
-            "4"
-          ],
-        },
-
-      ]).then((updateRole) => db.promise().query("UPDATE role_id WHERE first_name ?", updateRole).then(init)
+      db.query(
+        "SELECT concat(first_name,' ', last_name) name, id value FROM employee",
+        (err, empData) => {
+          db.query(
+            "SELECT title name, id value FROM role",
+            (err, updatedRole) => {
+              prompt([
+                {
+                  type: "list",
+                  name: "first_name",
+                  message: "Whose role do you want to update?",
+                  choices: empData,
+                },
+                {
+                  type: "list",
+                  name: "title",
+                  message:
+                    "Which role would you like to assign to the selected employee?",
+                  choices: updatedRole,
+                },
+              ]).then((updatedRole) => {
+                db.query(
+                  "UPDATE employee SET role_id = ? WHERE id = ?",
+                  [updatedRole.title, updatedRole.first_name],
+                  (err, data) => {
+                    init();
+                  }
+                );
+              });
+            }
+          );
+        }
       );
     }
   });
 };
 
 init();
-
